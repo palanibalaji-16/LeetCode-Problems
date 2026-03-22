72958577bd6d8cad52509d0b9be2045f874f5047
palanibalaji <palanibalajir.2005_bcs27@mepcoeng.ac.in>
2026-03-22 18:47:15 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.4 MB (9.84%)

--- solutions/0043-multiply-strings/solution.py
+++ solutions/0043-multiply-strings/solution.py
@@ -1,8 +1,5 @@
 class Solution:
     def multiply(self, num1: str, num2: str) -> str:
-        a=int(num1)
-        b=int(num2)
-        c=a*b
-        d=str(c)
-        return d
+        b=int(num1)*int(num2)
+        return str(b)
         
