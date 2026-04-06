8a29e95f5a78bedb29a19b1f1e3004023cfed02e
palanibalaji <palanibalajir.2005_bcs27@mepcoeng.ac.in>
2026-04-06 10:36:16 +0000

    Sync LeetCode submission Runtime - 2 ms (85.49%), Memory - 20.5 MB (24.66%)

--- solutions/0283-move-zeroes/solution.py
+++ solutions/0283-move-zeroes/solution.py
@@ -1,15 +1,12 @@
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
-        c=0
+        k=0
         for i in range(len(nums)):
-            if(nums[i]!=0):
-                nums[c]=nums[i]
-                c=c+1
-            
-        for j in range(c,len(nums)):
-            nums[j]=0
-    
-
-            
+            if nums[i]!=0:
+                nums[k]=nums[i]
+                k+=1
         
-
+        for i in range(k,len(nums)):
+            nums[i]=0
+        
+        
