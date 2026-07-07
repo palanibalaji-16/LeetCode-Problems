2e0866551be90a9a7a6eda997b5e57851b8ff111
palanibalaji <palanibalajir.2005_bcs27@mepcoeng.ac.in>
2026-07-07 06:10:52 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.4 MB (22.66%)

--- solutions/0075-sort-colors/solution.py
+++ solutions/0075-sort-colors/solution.py
@@ -1,4 +1,22 @@
 class Solution:
-    def sortColors(self, nums: List[int]) -> None:
-        nums.sort()
-        return nums
+    def sortColors(self, n: List[int]) -> None:
+        l=0
+        m=0
+        h=len(n)-1
+        while(h>=m):
+            if n[m]==0:
+                t=n[l]
+                n[l]=n[m]
+                n[m]=t
+                m=m+1
+                l=l+1
+            elif n[m]==1:
+                m=m+1
+            else:
+                t=n[m]
+                n[m]=n[h]
+                n[h]=t
+                h=h-1
+            
+        
+        
