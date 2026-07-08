2d7f5a23a3e7478e8c410e3f08d73e96af23de7c
palanibalaji <palanibalajir.2005_bcs27@mepcoeng.ac.in>
2026-07-08 09:09:44 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.4 MB (6.78%)

---
+++ solutions/1293-three-consecutive-odds/solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def threeConsecutiveOdds(self, a: List[int]) -> bool: 
+        c=0
+        for i in a:
+            if i%2!=0:
+                c=c+1
+                if c==3:
+                    return True
+            else:
+                c=0
+            
+        return False        
