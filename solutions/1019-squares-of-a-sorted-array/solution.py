f7e5d1fdc1bbec8e0431fbd861e89c5294021e7e
palanibalaji <palanibalajir.2005_bcs27@mepcoeng.ac.in>
2026-07-08 09:18:07 +0000

    Sync LeetCode submission Runtime - 3 ms (95.09%), Memory - 21.1 MB (28.08%)

--- solutions/1019-squares-of-a-sorted-array/solution.py
+++ solutions/1019-squares-of-a-sorted-array/solution.py
@@ -2,7 +2,7 @@ class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
         p=[]
         for i in nums:
-            p.append(pow(i,2))
+            p.append(pow(abs(i),2))
         
         p.sort()
         return p
