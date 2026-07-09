0d0694a7e3ac8df524a9400a61b13a1148837988
palanibalaji <palanibalajir.2005_bcs27@mepcoeng.ac.in>
2026-07-09 04:59:07 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.2 MB (91.44%)

--- solutions/3397-find-the-integer-added-to-array-i/solution.py
+++ solutions/3397-find-the-integer-added-to-array-i/solution.py
@@ -1,6 +1,3 @@
 class Solution:
-    def addedInteger(self, nums1: List[int], nums2: List[int]) -> int:
-        nums1.sort()
-        nums2.sort()
-        return nums2[0]-nums1[0]
-        
+    def addedInteger(self, n1: List[int], n2: List[int]) -> int:
+        return max(n2)-max(n1)
