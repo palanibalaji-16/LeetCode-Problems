3ef3908855bfb40ad29f035009470db76b107c8e
palanibalaji <palanibalajir.2005_bcs27@mepcoeng.ac.in>
2026-07-10 05:43:03 +0000

    Sync LeetCode submission Runtime - 3 ms (31.38%), Memory - 19.3 MB (34.63%)

---
+++ solutions/1950-sign-of-the-product-of-an-array/solution.py
@@ -0,0 +1,11 @@
+class Solution:
+    def arraySign(self, nums: List[int]) -> int:
+        s=1
+        for i in nums:
+            s=s*i
+        if (s>0):
+            return 1
+        elif (s==0):
+            return 0
+        else:
+            return -1
